f25df0372228dd48c0c84d8c952af4b372c00a94
shikao-yuzu <shikao.yuzu@gmail.com>
2019-02-04 00:01:38 +0900

    under working...

--- getcal-antlers.py
+++ getcal-antlers.py
@@ -1,3 +1,4 @@
+import csv
 from bs4 import BeautifulSoup
 import requests
 
@@ -20,7 +21,26 @@ def main():
     # BeautifulSoupオブジェクトの生成
     soup = BeautifulSoup(html, 'html.parser')
 
-    print(soup.select('.game'))
+    # 出場大会一覧のリスト
+    competitions = soup.findAll(name='div', attrs={'class': 'title_name_m0'})
+    for c in competitions:
+        print(c.string)
+
+    # とりあえずCSV出力
+    table = soup.findAll(name="table")[0]
+    rows = table.findAll(name="tr")
+
+    csvFile = open("test.csv", 'wt', newline = '', encoding = 'shift_jis')
+    writer = csv.writer(csvFile)
+
+    try:
+        for row in rows:
+            csvRow = []
+            for cell in row.findAll(name=['td', 'th']):
+                csvRow.append(cell.get_text().strip())
+            writer.writerow(csvRow)
+    finally:
+        csvFile.close()
 
 
 if __name__ == '__main__':
