f70042be91427216d1d7db8cb11d73f8e5a7f24b
shikao-yuzu <shikao.yuzu@gmail.com>
2019-02-03 21:55:13 +0900

    under working

--- getcal-antlers.py
+++ getcal-antlers.py
@@ -1,11 +1,26 @@
 from bs4 import BeautifulSoup
+import requests
+
+
+# 取得対象のWebサイト
+URL_ANTLERS_CAL = 'http://www.so-net.ne.jp/antlers/games'
+
 
 def main():
     """
     @brief:
       メイン関数
     """
-    print("hoge")
+    # Webサイトにgetリクエストで送信し情報を取得
+    r = requests.get(URL_ANTLERS_CAL)
+
+    # HTTPレスポンスボディを取得
+    html = r.text
+
+    # BeautifulSoupオブジェクトの生成
+    soup = BeautifulSoup(html, 'html.parser')
+
+    print(soup.select('.game'))
 
 
 if __name__ == '__main__':
