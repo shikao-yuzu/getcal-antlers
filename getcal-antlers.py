281ae7daca5dec0d0693210a3ff893a9799c96c7
shikao-yuzu <shikao.yuzu@gmail.com>
2019-10-22 16:50:23 +0900

    HPの仕様変更に対応

--- getcal-antlers.py
+++ getcal-antlers.py
@@ -23,13 +23,13 @@ def output_game_schedule(soup: BeautifulSoup, competitions: list, idx_comp: int)
       試合情報(HTML)をパースしてGoogleカレンダー用のCSVに出力
     """
     # テーブルの取得
-    tables = soup.findAll(name='table')
+    tables = soup.findAll(name='div', attrs={'class': 'game-index-wrap'})
 
     # 大会名
     comp = competitions[idx_comp]
 
     # 試合リスト
-    game_lists = tables[idx_comp].findAll(name='tr')
+    game_lists = tables[idx_comp].findAll(name='div', attrs={'class': 'gameScheduleList__item'})
 
     # 出力ファイルの再オープン
     csvFile = open(OUTPUT_PATH, 'at', newline = '', encoding = 'shift_jis')
@@ -38,30 +38,22 @@ def output_game_schedule(soup: BeautifulSoup, competitions: list, idx_comp: int)
     # パース＆出力
     try:
         for idx_game, game in enumerate(game_lists):
-            # 表ラベルは不要
-            if idx_game == 0:
-                continue
-
-            parsed_game = []
-            for idx_cell, cell in enumerate(game.findAll(name=['td', 'th'])):
-                s = cell.get_text().strip().replace('\n','')
-
-                # 中継以降の列は飛ばす
-                if idx_cell > 3:
-                    continue
-
-                # 全角英数字を半角に変換
-                s_z2h = jaconv.z2h(s, kana=False, digit=True, ascii=True)
-
-                parsed_game.append(s_z2h)
-
             # 節
-            sec = parsed_game[0]
+            sec = game.find(name='p', attrs={'class': 'gameScheduleList__season'})
+            if sec == None:
+                sec = ''
+            else:
+                sec = sec.get_text().strip()
+                sec = jaconv.z2h(sec, kana=False, digit=True, ascii=True)
 
-            # 日時
-            date_time = re.split('[or +)(※]', parsed_game[1])
-            day       = date_time[0]
-            s_time    = date_time[-1]
+            # 日
+            day = game.find(name='p', attrs={'class': 'gameScheduleList__date'}).get_text().strip()
+            day = jaconv.z2h(day, kana=False, digit=True, ascii=True)
+            day = re.split('[or +)(※]', day)
+            day = day[0].replace('.', '/')
+
+            # 開始時刻
+            s_time = game.find(name='p', attrs={'class': 'gameScheduleList__time'}).get_text().strip()
 
             # 終日イベントフラグと終了時刻の設定
             if s_time == '未定':
@@ -75,10 +67,15 @@ def output_game_schedule(soup: BeautifulSoup, competitions: list, idx_comp: int)
                 e_time       = e_time_dt.strftime('%H:%M')
 
             # 対戦相手
-            team = parsed_game[2]
+            team = game.find(name='span', attrs={'class': 'gameScheduleList__clubName'}).get_text().strip()
+            team = jaconv.z2h(team, kana=False, digit=True, ascii=True)
 
             # スタジアム
-            stadium = parsed_game[3]
+            stadium = game.find(name='p', attrs={'class': 'gameScheduleList__stadium'}).get_text().strip()
+            stadium = jaconv.z2h(stadium, kana=False, digit=True, ascii=True)
+            stadium = stadium.strip('HOME')
+            stadium = stadium.strip('AWAY')
+            stadium = stadium.strip('\n')
 
             csvRow = []
             csvRow.append(comp+' '+sec+' '+team)
@@ -106,27 +103,14 @@ def main():
     # HTTPレスポンスボディを取得
     html = r.text
 
-    # 取得元のHTMLタグが壊れていて読み込みに失敗するため，パース前にHTMLファイルの該当行を削除
-    # (2019/07/07時点)
-    html_mod = ''
-    for s in html.splitlines():
-        if '</a></span></li</ul>' in s:
-            pass
-        else:
-            html_mod += s + os.linesep
-
     # BeautifulSoupオブジェクトの生成
-    soup = BeautifulSoup(html_mod, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser')
 
     # 出場大会のリストを構築
     competitions = []
-    for c in soup.findAll(name='div', attrs={'class': 'title_name_m0'}):
+    for c in soup.findAll(name='h3', attrs={'class': 'title -style05_01'}):
         # 全角英数字を半角に変換
         c_z2h = jaconv.z2h(c.string, kana=False, digit=True, ascii=True)
-
-        # 除外する大会等
-        if c_z2h == '過去の試合結果':
-            continue
 
         competitions.append(c_z2h)
 
